c0ac8ad017510fa1606a740e9a0c71599f97eb46
Mathew Padley <git@hewley.co.uk>
2024-10-29 15:36:58 -0700

    Add initial project configuration and test results files

--- DartsScorer/models/GameType.cs
+++ DartsScorer/models/GameType.cs
@@ -2,7 +2,7 @@ namespace DartsScorer.models;
 
 public class GameType
 {
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public int StartScore { get; set; }
     
 }
